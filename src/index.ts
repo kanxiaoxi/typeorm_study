fb62b105b8bf67ecbb912148206a577abaa1f300
kanxiaoxi <wzxjachinwu@gmail.com>
2021-12-17 14:17:38 +0800

    连接Postgres数据库

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world')
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "wuxiaoshuai",
+      password: "password",
+      database: "typeorm_study"
+    })
+    console.log('成功连接到Postgres数据库')
+  } catch(error) {
+    console.error(error)
+    throw new Error('连接数据库失败')
+  }
+}
+
+main()
