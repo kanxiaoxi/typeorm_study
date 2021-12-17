d7f35118b0545873f999a73e1d75bfcf89d09cce
kanxiaoxi <wzxjachinwu@gmail.com>
2021-12-17 18:43:30 +0800

    创建添加数据的路由

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { createConnection } from 'typeorm'
-import { Banker } from './entities/Banker'
-import { Client } from './entities/Client'
-import { Transaction } from './entities/Transaction'
+import { createConnection } from "typeorm";
+import express from "express";
+import { Banker } from "./entities/Banker";
+import { Client } from "./entities/Client";
+import { Transaction } from "./entities/Transaction";
+import { createClientRouter } from "./routes/create_client";
+import { createBankerRouter } from "./routes/create_banker";
+
+const app = express();
 
 const main = async () => {
   try {
@@ -12,14 +17,23 @@ const main = async () => {
       username: "wuxiaoshuai",
       password: "password",
       database: "typeorm_study",
-      entities: [Client,Banker,Transaction],
-      synchronize: true
-    })
-    console.log('成功连接到Postgres数据库')
-  } catch(error) {
-    console.error(error)
-    throw new Error('连接数据库失败')
-  }
-}
+      entities: [Client, Banker, Transaction],
+      synchronize: true,
+    });
+    console.log("成功连接到Postgres数据库");
 
-main()
+    app.use(express.json());
+    app.use(createClientRouter);
+    app.use(createBankerRouter);
+
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`服务器运行在${PORT}端口`);
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("连接数据库失败");
+  }
+};
+
+main();
